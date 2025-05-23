4abae28993949f26414f845c2e64c67cc6f0a625
MohdZubair <mohmmedzubair6786@gamil.com>
2025-05-23 18:47:02 +0530

    remove logo

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -147,8 +147,8 @@ const Navbar = () => {
                 ? 'bg-gradient-to-r from-pink-600 to-purple-700 bg-clip-text text-transparent' 
                 : 'text-white drop-shadow-md'
             }`}>
-              <img src="/vite.jpg" alt="Logo" className="w-12 h-12 mr-2 rounded" />
-              {/* Find<span className={scrolled ? 'text-pink-500' : 'text-pink-300'}>My</span>Venue */}
+              {/* <img src="/vite.jpg" alt="Logo" className="w-12 h-12 mr-2 rounded" /> */}
+              Find<span className={scrolled ? 'text-pink-500' : 'text-pink-300'}>My</span>Venue
             </span>
           </Link>
 
